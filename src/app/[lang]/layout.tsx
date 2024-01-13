abbbe83712c859282dec6e692da6f045345db31f
Bonea Adrian <boneamirel@gmail.com>
2024-01-13 20:51:59 +0200

    Add remixicon.css import to layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import 'remixicon/fonts/remixicon.css';
 import type { Metadata } from 'next';
 import { Fira_Code } from 'next/font/google';
 import { Footer, Navbar } from '@/components';
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,3 @@
-import 'remixicon/fonts/remixicon.css';
-
 export default function Page() {
   return <h1 className='text-headline'>TEST</h1>;
 }
