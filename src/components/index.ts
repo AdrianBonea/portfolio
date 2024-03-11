772f6b7d6219b61bef6cf8f129f2458774936940
Bonea Adrian <boneamirel@gmail.com>
2024-03-11 21:34:17 +0200

    Add HamburgerMenu component and update next.config.js

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { HambugerMenu } from './HamburgerMenu/HamburgerMenu';
 export { Navbar } from './Shared/Navbar/Navbar';
 export { Footer } from './Shared/Footer/Footer';
 export { Homepage } from './Homepage/Homepage';
