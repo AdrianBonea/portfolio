3188b549288e847aa897acb47349f23de0b3cedd
Bonea Adrian <boneamirel@gmail.com>
2024-05-28 22:54:41 +0300

    remove unused styles

--- src/components/Shared/Icons/HamburgerIcon.tsx
+++ src/components/Shared/Icons/HamburgerIcon.tsx
@@ -4,7 +4,7 @@ export const HamburgerIcon = (): JSX.Element => {
   const open = useMenuStore((state) => state.menuIsOpen);
   return (
     <i
-      className={`${open ? 'ri-menu-5-fill' : 'ri-menu-fill'} px-8 py-5 text-body transition-transform duration-500 ease-in-out hover:border-b-accent-orange hover:text-white`}
+      className={`${open ? 'ri-menu-5-fill' : 'ri-menu-fill'} px-8 py-5 hover:border-b-accent-orange hover:text-white`}
     />
   );
 };
